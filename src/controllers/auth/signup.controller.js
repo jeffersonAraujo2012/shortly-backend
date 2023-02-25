a3a474931bcf61f7c021ae5971f11a8f4c4f5b79
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-02-25 15:32:52 -0300

    Implemented: user registration.

--- src/controllers/auth/signup.controller.js
+++ src/controllers/auth/signup.controller.js
@@ -1,7 +1,31 @@
-export default async function signup(req, res) {
-  //Verificar se já existe algum usuário cadastrado com
-  //o email fornecido
+import bcrypt from 'bcrypt';
+import db from '../../database/database.js';
 
-  //Cadastrar novo usuário
+export default async function signup(req, res) {
+  const PEPPER = process.env.PEPPER | '$any_pepper$';
+  const newUser = req.body;
+
+  newUser.password = bcrypt.hashSync(newUser.password + PEPPER, 10);
+  newUser.email = bcrypt.hashSync(newUser.email + PEPPER, 10);
+
+  try {
+    const resultQuery = await db.query('SELECT * FROM users WHERE email = $1', [
+      newUser.email,
+    ]);
+    if (resultQuery.rowCount > 0) {
+      return res.sendStatus(409);
+    }
+
+    const resultSignup = await db.query(
+      `
+      INSERT INTO users (name, email, password)
+      VALUES ($1, $2, $3)
+    `,
+      [newUser.name, newUser.email, newUser.password]
+    );
+  } catch (error) {
+    return res.status(500).send('Algo de errado no processo de cadastro.');
+  }
+
   res.sendStatus(201);
 }
