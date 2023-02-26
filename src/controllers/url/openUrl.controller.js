f78b37fa2a01aeddeb9e793ac524fcc7e22070c9
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-02-26 00:53:10 -0300

    Fixed: counter of access.

--- src/controllers/url/openUrl.controller.js
+++ src/controllers/url/openUrl.controller.js
@@ -13,6 +13,11 @@ export default async function openShortLink(req, res) {
       return res.sendStatus(404);
     }
 
+    await db.query('UPDATE urls SET "visitCount" = $1, "accessedAt" = now() WHERE id = $2', [
+      resultQueryUrl.rows[0].visitCount + 1,
+      resultQueryUrl.rows[0].id
+    ]);
+
     return res.status(300).redirect(resultQueryUrl.rows[0].url);
   } catch (error) {
     return res.status(500).send('Internal Error: ' + error.message);
